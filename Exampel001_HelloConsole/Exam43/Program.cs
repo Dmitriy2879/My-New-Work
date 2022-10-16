c6e56c135c969aff7c944a4a549a64e6ca5da4e9
Dmitriy <rubagelen@gmail.com>
2022-10-16 20:23:51 +0300

    add Exam43

---
+++ Exampel001_HelloConsole/Exam43/Program.cs
@@ -0,0 +1,15 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+double Factorial(int n)
+{
+    if (n == 1)
+        return 1; // 1!=1  0!=1
+    else
+        return n * Factorial(n - 1);
+}
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"{i}!={Factorial(i)}");
+}
